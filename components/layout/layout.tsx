ad6ee84ff16accdeab6eab6b200df4c7c04ff84e
Rashid Mamarimov <sonochiwaxd@gmail.com>
2022-05-23 19:50:39 +0600

    layout hotfix

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -16,7 +16,7 @@ export default function ({ children, title }: ILayout) {
     <>
       <Head>
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title} | Gscore</title>2
       </Head>
